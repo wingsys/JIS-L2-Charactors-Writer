68e7132f7cc4eef5f68e956c79bbeffde1488cd5
Osamu SUGIYAMA <rei.0.000000@gmail.com>
2017-09-26 22:07:40 +0900

    add indexes
    add indexes by enumerate.
    output dirs like {output_dir}/{index}_{charactor}/image{index}.png

--- script.py
+++ script.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import json
+import os
 from glob import glob
 from itertools import product
 from sys import argv
 from PIL import Image, ImageDraw, ImageFont
 
 
-def write(char: str, font: ImageFont.FreeTypeFont):
+def write(char, font):
     canvas = Image.new("L", (prop["size"]["width"], prop["size"]["height"]), 255) # grayscale, white bg
-    ImageDraw.Draw(canvas).text((0, 0), char, font=font, fill="#000")
-    canvas.save("{}/{}-{}.png".format(prop["output_dir"], font.getname()[0], char), "PNG")
+    ImageDraw.Draw(canvas).text((0, 0), char[1][0], font=font[1], fill="#000")
+    output = "{0}/{1:04d}_{2}".format(prop["output_dir"], char[0], char[1][0])
+    os.makedirs(output, mode=0o777, exist_ok=True)
+    canvas.save("{0}/image{1:02d}.png".format(output, font[0]), "PNG")
 
 with open(argv[1], "r") as rawprop:
     prop = json.load(rawprop)
 fonts = [ImageFont.truetype(e, prop["size"]["font"]) for e in glob(prop["fonts"])]
 
-with open(prop["charfile"], "r") as chars:
-    for i in product(chars, fonts):
-        print(i[0][0])
-        write(i[0][0], i[1])
+with open(prop["charfile"], "r", encoding='utf-8') as chars:
+    for i in product(enumerate(chars, start=1), enumerate(fonts, start=1)):
+        print(i[0][1][0])
+        write(i[0], i[1])
